cad3e9f292aaadba0a0d9b2346ef74bd548f99c4
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-08-23 23:15:41 +0900

    AkitaTest敵挙動と土魔法を改善

--- Assets/AkitaTest/magic/tutiMagic.cs
+++ Assets/AkitaTest/magic/tutiMagic.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class tutiMagic : MonoBehaviour
 {
+    public GameObject magicWallTest;
     public GameObject magicWallMihon;
     public GameObject magicWall;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,21 +18,21 @@ public class tutiMagic : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Z))
         {
-            Vector3 tPosition = (transform.position + transform.forward*2f);
-            float x = Mathf.Round(tPosition.x / 1.5f);
-            float z = Mathf.Round(tPosition.z / 1.5f);
-            Vector3 magicWallPosition = new Vector3(x*1.5f,0.5f,z*1.5f);
-            Instantiate(magicWallMihon, magicWallPosition, Quaternion.Euler(transform.forward));
-        }
-        if (Input.GetKeyUp(KeyCode.Z))
-        {
             Vector3 tPosition = (transform.position + transform.forward * 2f);
             float x = Mathf.Round(tPosition.x / 1.5f);
             float z = Mathf.Round(tPosition.z / 1.5f);
             Vector3 magicWallPosition = new Vector3(x * 1.5f, 0.5f, z * 1.5f);
-            Instantiate(magicWall, magicWallPosition, Quaternion.Euler(transform.forward));
+            Instantiate(magicWallMihon, magicWallPosition, Quaternion.Euler(transform.forward)); 
         }
-
-
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+               Vector3 tPosition = (transform.position + transform.forward * 2f);
+               float x = Mathf.Round(tPosition.x / 1.5f);
+               float z = Mathf.Round(tPosition.z / 1.5f);
+               Vector3 magicWallPosition = new Vector3(x * 1.5f, 0.5f, z * 1.5f);
+               Instantiate(magicWall, magicWallPosition, Quaternion.Euler(transform.forward));
+        }
+          
     }
 }
+
---
+++ Assets/AkitaTest/magic/tutiWallTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutiWallTest : MonoBehaviour
+{
+    GameObject tutiWallMihon;
+    GameObject tutiWall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(tutiWallMihon,transform.position, Quaternion.Euler(transform.forward));
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            Debug.Log("壊れるよ");
+            Destroy(this.gameObject);
+        }
+    }
+    public void setObj(GameObject tWMihon,GameObject tW)
+    {
+        tutiWallMihon = tWMihon;
+        tutiWall = tW;
+    }
+}
