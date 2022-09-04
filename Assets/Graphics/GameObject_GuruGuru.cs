4b53259622a4c6bb5630b7848b3da3f4e52b31b5
unknown <[erou1227@yahoo.co.jp]>
2022-09-05 07:52:29 +0900

    木村：ゲームオーバーシーン実装中

---
+++ Assets/Graphics/GameObject_GuruGuru.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObject_GuruGuru : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<CanvasGroup>().alpha = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Fukidashi_Active(){
+        this.gameObject.GetComponent<CanvasGroup>().alpha = 1;
+    }
+}
