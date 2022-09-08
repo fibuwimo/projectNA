1d875da600ecbbe53886fe49d457f9e6d947e5aa
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-09-08 09:20:15 +0900

    ゲッター更新

---
+++ Assets/AkitaTest/PinkTest/ResultScoreController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultScoreController : MonoBehaviour
+{
+    public Text resultScoreT;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultScoreT.text = "すこあ"+PlayerControllerKai.getResultScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
