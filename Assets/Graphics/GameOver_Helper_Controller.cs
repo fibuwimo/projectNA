f4cd9a58c962b42f47518949f71a9e7aae446658
unknown <[erou1227@yahoo.co.jp]>
2022-09-09 07:36:05 +0900

    木村：ゲームオーバー調整中

--- Assets/Graphics/GameOver_Helper_Controller.cs
+++ Assets/Graphics/GameOver_Helper_Controller.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class GameOver_Helper_Controller : MonoBehaviour
 {
     public GameObject Meido;
+    public GameObject RC;
     Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         Meido = GameObject.Find("GameOver_meido");
+        RC = GameObject.Find("GameOver_RC");
         anim = gameObject.GetComponent<Animator>();
     }
 
@@ -23,5 +25,8 @@ public class GameOver_Helper_Controller : MonoBehaviour
     }
     public void Surprized_Meido(){
         Meido.GetComponent<GameOver_Meido_Controller>().End_FirstRoop_Anim();
+    }
+    public void Push_RC(){
+        RC.GetComponent<GameOver_RC>().RC_Active();
     }
 }
---
+++ Assets/Graphics/GameOver_RC.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver_RC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<CanvasGroup>().alpha = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RC_Active(){
+        this.gameObject.GetComponent<CanvasGroup>().alpha = 1;
+    }
+
+    public void RC_InActive(){
+        Destroy(gameObject);
+        //this.gameObject.GetComponent<CanvasGroup>().alpha = 0;
+    }
+}
